54b713beb842f9dd2a2b2e8fd42d2c5285ceeecf
butterballhi <dindputr15@gmail.com>
2026-02-27 00:05:26 +0700

    feat: add all routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,16 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->group('api', function($routes) {
+    $routes->resource('kategori', ['controller' => 'Api\KategoriController']);
+    $routes->resource('kuda-kuda', ['controller' => 'Api\KudaKudaController']);
+    $routes->resource('tendangan', ['controller' => 'Api\TendanganController']);
+    $routes->resource('pukulan', ['controller' => 'Api\PukulanController']);
+    $routes->resource('tangkisan', ['controller' => 'Api\TangkisanController']);
+    $routes->resource('poomsae', ['controller' => 'Api\PoomsaeController']);
+    $routes->resource('kyorugi-info', ['controller' => 'Api\KyorugiInfoController']);
+    $routes->resource('kelas-berat', ['controller' => 'Api\KelasBeratController']);
+    $routes->resource('kyorugi-peralatan', ['controller' => 'Api\KyorugiPeralatanController']);
+    $routes->resource('open-kyorugi-info', ['controller' => 'Api\OpenKyorugiInfoController']);
+});
