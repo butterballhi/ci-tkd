022d45b3971e1b841ec14ccf3806b89627f7151a
butterballhi <dindputr15@gmail.com>
2026-02-26 21:38:52 +0700

    refactor: rename KudakudaModel to KudaKudaModel

--- app/Models/KudakudaModel.php
+++ app/Models/KudakudaModel.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class KudakudaModel extends Model
+class KudaKudaModel extends Model
 {
     protected $table            = 'kuda_kuda';
     protected $primaryKey       = 'id';
